a596abe35e9918f93bd3236db39248054ccdc4ee
OsKaLis <oskalis@yandex.ru>
2022-06-03 20:17:25 +0300

    ver 00.0

---
+++ alien_invasion.py
@@ -0,0 +1,68 @@
+import pygame
+from pygame.sprite import Group
+
+# Свои модули
+from settings import Settings
+from ship import Ship
+import game_functions as gf
+from alien import Alien
+from game_stats import GameStats
+from button import Button
+from scoreboard import Scoreboard
+
+def run_game():
+    # Инициализирует игру и создает обьект экрана.
+    pygame.init()
+    ai_settings = Settings()
+    screen = pygame.display.set_mode(
+        (ai_settings.screen_width, ai_settings.screen_height))
+    pygame.display.set_caption('Alien Invasion')
+
+    # Создание космо коробля
+    ship = Ship(ai_settings, screen)
+
+    # Создание пули для хранения пуль.
+    bullets = Group()
+
+    # Создание пришельца.
+    #alien = Alien(ai_settings, screen)
+
+    # Создание групы пришельцев.
+    aliens = Group()
+
+    # Создание Флота пришельцев
+    gf.create_fleet(ai_settings, screen, ship, aliens)
+
+    # Создаём экзмпляр для хранения игровой статистики.
+    stats = GameStats(ai_settings)
+
+    # Создание кнопки Play.
+    play_button = Button(ai_settings, screen, "Игра")
+
+    # Создание экземпляров GameStats и Scoreboard.
+    sb = Scoreboard(ai_settings, screen, stats)
+
+    # Запуск основной цикла игры.
+    while True:
+        # Отслеживаем события клавиатуры и мыши
+        gf.check_events(ai_settings, screen, stats, sb,
+            play_button, ship, bullets, aliens)
+
+        if stats.game_active:
+            # Обновление нажатий клавиш
+            ship.update()
+            # Обновление позиции пуль и уничтожает старые пули
+            gf.update_bullets(ai_settings, screen, stats, sb, ship, aliens,
+                bullets)
+            # Обработка колизий удалять сталкнувшихся пуль с пришельцами
+            gf.check_bullet_alien_collisions(ai_settings, screen, stats, sb,
+                ship, aliens, bullets)
+            # Обновление позиции пришельцев
+            gf.update_aliens(ai_settings, screen, stats, sb, ship, aliens,
+                bullets)
+
+        # Обновляет изображения на экране и отображает новый экран.
+        gf.update_screen(ai_settings, screen,
+            stats, sb, ship, aliens, bullets, play_button)
+
+run_game()
---
+++ settings.py
@@ -0,0 +1,61 @@
+class Settings():
+    """
+    Класс для хранения всех настроек игры Alien Invasion.
+    """
+
+    def __init__(self):
+        """
+        Инициализируем настройки игры.
+        """
+        self.screen_width = 900
+        self.screen_height = 500
+        self.bg_color = (230, 230, 230)
+        #del self.ship_speed_factor = 1.5
+
+        # Настройки коробля
+        self.ship_limit = 3
+
+        # Параметры пули
+        #del self.bullet_speed_factor = 3
+        self.bullet_width = 3
+        self.bullet_height = 15
+        self.bullet_color = (240, 20, 20)
+        self.bullets_allowed = 3
+
+        # Настройка пришельцев
+        #del self.alien_speed_factor = 1
+        self.fleet_drop_speed = 10
+        # fleet_direction = 1 обозначает движение в право, а -1 - влево.
+        #del self.fleet_direction = 1
+
+        # Темп ускорения игры
+        self.speedup_scale = 1.1
+
+        # Темп роста стоимости пришельцев
+        self.score_scale = 1.5
+        self.initialize_dynamic_settings()
+
+    def initialize_dynamic_settings(self):
+        """
+        Инициализируем настройки, изменяющиеся в ходе игры.
+        """
+        self.ship_speed_factor = 1.5
+        self.bullet_speed_factor = 3
+        self.alien_speed_factor = 1
+
+        # fleet_direction = 1 обозначает движение в право, а -1 - влево.
+        self.fleet_direction = 1
+
+        # Подсчет очков
+        self.alien_points = 50
+
+    def increase_speed(self):
+        """
+        Увеличивает настройки скорости и стоимости пришельцев.
+        """
+        self.ship_speed_factor *= self.speedup_scale
+        self.bullet_speed_factor *= self.speedup_scale
+        self.alien_speed_factor *= self.speedup_scale
+        self.alien_points = int(self.alien_points * self.score_scale)
+        # Показывает увеличиную стоимоть пришельца
+        #print(self.alien_points)
